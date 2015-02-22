da2e0ef7965f9784ee4a369d0e4c42148cb4d88c
Nishith Khandwala <nishith@stanford.edu>
2015-02-22 10:32:50 -0800

    Implemented code to obtain frames from videos

---
+++ scripts/data_preprocessing/process_video.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+video = cv2.VideoCapture('../data/pool.mp4')
+
+success, image = video.read()
+
+count = 0
+while success:
+	success, image = video.read()
+	cv2.imwrite("frame%d.jpg" % count, image)
+	if cv2.waitKey(10) == 27:
+		break
+	count += 1
+
+
